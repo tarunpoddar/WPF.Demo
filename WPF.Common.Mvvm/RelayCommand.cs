b326ea32a065d1aac0df74a71be7aec38921f92b
tarunpoddar <tarun.tarun05@gmail.com>
2023-08-15 22:32:03 +0530

    Added Commands and interaction triggers for autocomplete in textbox.

---
+++ WPF.Common.Mvvm/RelayCommand.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Input;
+
+namespace WPF.Common.Mvvm
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private readonly Action<object>? action;
+        private readonly Predicate<object>? canExecute;
+        private Action<string> selectSuggestion;
+
+        public RelayCommand(Action action, Func<bool>? canExecute = null)
+        {
+            this.action = action != null ? (Action<object>)(obj => action()) : throw new ArgumentNullException(nameof(action));
+            if (canExecute == null)
+                return;
+            this.canExecute = (Predicate<object>)(obj => canExecute());
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            Predicate<object> canExecute = this.canExecute;
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter) => this.action(parameter);
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Action<T> execute)
+           : this(execute, null)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        //public event EventHandler CanExecuteChanged
+        //{
+        //    add { CommandManager.RequerySuggested += value; }
+        //    remove { CommandManager.RequerySuggested -= value; }
+        //}
+    }
+}
--- WPF.Demo/MainWindow/ViewModel/MainWindowViewModel.cs
+++ WPF.Demo/MainWindow/ViewModel/MainWindowViewModel.cs
@@ -1,18 +1,108 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 using WPF.Common.Mvvm;
 
 namespace WPF.Demo.MainWindow.ViewModel
 {
-    class MainWindowViewModel: ObservableObject
+    class MainWindowViewModel : ObservableObject
     {
         public ICommand? CreateCommand { get; set; }
         public ICommand? ReadCommand { get; set; }
         public ICommand? UpdateCommand { get; set; }
         public ICommand? DeleteCommand { get; set; }
+        public ICommand? SubmitCommand { get; set; }
+        public ICommand? MouseDownCommand { get; set; }
 
+        /// <summary>
+        /// Customer's name.
+        /// </summary>
+        private string? aName;
+
+        public string? Name
+        {
+            get => aName;
+            set => SetProperty(ref aName, value);
+        }
+
+        /// <summary>
+        /// Collection of Genders.
+        /// </summary>
+        public ObservableCollection<string> Genders { get; set; }
+
+        /// <summary>
+        /// The selected gender.
+        /// </summary>
+        private string? aSelectedGender;
+
+        public string? SelectedGender
+        {
+            get => aSelectedGender;
+            set => SetProperty(ref aSelectedGender, value);
+        }
+
+        public ObservableCollection<string> Products { get; } = new ObservableCollection<string>();
+
+        private ObservableCollection<string>? _filteredSuggestions;
+        public ObservableCollection<string>? FilteredSuggestions
+        {
+            get => _filteredSuggestions;
+            set => SetProperty(ref _filteredSuggestions, value);
+        }
+
+        private string? aSearchProduct;
+
+        public string? SearchProduct
+        {
+            get => aSearchProduct;
+            set
+            {
+                SetProperty(ref aSearchProduct, value);
+                UpdateFilteredSuggestions();
+            }
+        }
+
+        public ICommand SelectSuggestionCommand { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public MainWindowViewModel()
         {
-           // CreateCommand = new RelayCommand
+            SelectSuggestionCommand = new RelayCommand<string>(SelectSuggestion);
+            FilteredSuggestions = new ObservableCollection<string>();
+
+            // CreateCommand = new RelayCommand
+            Name = string.Empty;
+            Genders = new ObservableCollection<string>()
+            {
+                "Select", "Male", "Female", "Others"
+            };
+            SelectedGender = Genders.FirstOrDefault();
+
+            Products = new ObservableCollection<string>()
+            {
+                "Cement", "Rod", "Bali", "Pathor", "Taar"
+            };
+        }
+
+        private void UpdateFilteredSuggestions()
+        {
+            if (SearchProduct != null)
+            {
+                var filteredSuggestions = Products.Where(s => s.Contains(SearchProduct, System.StringComparison.OrdinalIgnoreCase));
+                FilteredSuggestions = new ObservableCollection<string>(filteredSuggestions);
+            }
+        }
+
+        /// <summary>
+        /// This is called on selection of a text in the popup.
+        /// </summary>
+        /// <param name="suggestion"></param>
+        private void SelectSuggestion(string suggestion)
+        {
+            SearchProduct = suggestion;
+            FilteredSuggestions?.Clear();
         }
     }
 }
