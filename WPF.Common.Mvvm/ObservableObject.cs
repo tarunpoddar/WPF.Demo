4bbcfe17837a982ca9ca30169889506f408ec9bc
tarunpoddar <tarun.tarun05@gmail.com>
2023-08-15 14:23:15 +0530

    Added a common project for code sharing and added controls.

---
+++ WPF.Common.Mvvm/ObservableObject.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF.Common.Mvvm
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property values are changed.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        /// <summary>
+        /// Must be called when a property value changed.
+        /// </summary>
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Helper method to set the property value and call OnPropertyChanged.
+        /// </summary>
+        /// <returns>True if the property changed, false otherwise</returns>
+        protected bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (property?.Equals(value) ?? false)
+            {
+                return false;
+            }
+
+            property = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
--- WPF.Demo/MainWindow/ViewModel/MainWindowViewModel.cs
+++ WPF.Demo/MainWindow/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
+using WPF.Common.Mvvm;
 
 namespace WPF.Demo.MainWindow.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel: ObservableObject
     {
+        public ICommand? CreateCommand { get; set; }
+        public ICommand? ReadCommand { get; set; }
+        public ICommand? UpdateCommand { get; set; }
+        public ICommand? DeleteCommand { get; set; }
 
+        public MainWindowViewModel()
+        {
+           // CreateCommand = new RelayCommand
+        }
     }
 }
